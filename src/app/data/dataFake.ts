735fd5911c3b9f8ca700f64b6c0a09ad71755546
José Mateus <josemateus@alu.ufc.br>
2023-10-02 20:59:37 -0300

    Tornando a content page dinâmica

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
-    "id":1,
-    "photo": "aaa.com",
+    "id":"1",
     "title": "saiu o filme",
-    "description": "ola"
+    "description": "ola",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
   {
-    "id":1,
-    "photo": "aaa.com",
-    "title": "saiu o filme",
-    "description": "ola"
+    "id":"2",
+    "title": "Nova série anunciado no Disney+",
+    "description": "ola",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
   }
 ]
